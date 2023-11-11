25582d51c13cca4d7526eea1c93f85eabaacb608
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-11 10:40:30 +0300

    connect to mongodb using mongoose connect method

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
+//connection to mongodb
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log("Server is running on port 3000");
 });
