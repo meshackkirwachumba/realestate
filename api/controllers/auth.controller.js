55ca913b8de8e8fe97e56f777cd53ac450ac00fb
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-12 18:53:43 +0300

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import Jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,4 +17,38 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = await req.body;
+
+  const validUser = await User.findOne({ email });
+
+  if (!validUser) {
+    return next(errorHandler(404, "User not found"));
+  }
+
+  const isMatch = await bcryptjs.compare(password, validUser.password);
+
+  if (!isMatch) {
+    return next(errorHandler(401, "Invalid credentials"));
+  }
+
+  //create a login token
+  const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  //remove the password from the response
+  const { password: removedPassword, ...validUserWithoutPassword } =
+    validUser._doc;
+
+  //save the token in a cookie
+  res
+    .cookie("access_token", token, {
+      httpOnly: true,
+      expiresIn: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    })
+    .status(200)
+    .json(validUserWithoutPassword);
 };
