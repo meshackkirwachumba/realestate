e7055578737689f760b2089b2378860ed171e12e
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-12 22:39:31 +0300

    added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ToastProvider from "./providers/ToastProvider.jsx";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastProvider />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <ToastProvider />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
