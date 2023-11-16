becc869784893b96de9d5e514a147c6da25e8fa1
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-16 17:12:26 +0300

    add pre-installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -28,10 +29,18 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+const __dirname = path.resolve();
+
 //Routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //error sent to middleware to handle it
 app.use((err, req, res, next) => {
