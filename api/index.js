12cef435693729e37982d8f9afcca16cff0f60ba
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-11-16 18:09:22 +0300

    unique index issue fixed

--- api/index.js
+++ api/index.js
@@ -8,30 +8,27 @@ import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
 const app = express();
 
-//allow client to send input data in json format
 app.use(express.json());
 
 app.use(cookieParser());
 
-//connection to mongodb
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
-const __dirname = path.resolve();
-
-//Routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
@@ -42,15 +39,12 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-//error sent to middleware to handle it
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message: message,
-    stack: err.stack,
+    message,
   });
 });
